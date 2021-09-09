588e4244a9c46dab0324b4bd9f198488a339e652
Denis Mythnik <themrcrovvley@gmail.com>
2021-09-09 16:22:04 +0300

    feat: add solce to transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,70 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const newArr = [];
+  for (let idx = 0; idx < arr.length; idx++) {
+    if (arr[idx] === '--discard-next') {
+      if (arr[idx + 1]) {
+        console.log('here');
+        idx++;
+      }
+    } else if (arr[idx] === '--discard-prev') {
+      if (arr[idx - 1] && arr[idx - 2] !== '--discard-next') {
+        newArr.pop();
+      }
+    } else if (arr[idx] === '--double-next') {
+      if (arr[idx + 1]) {
+        newArr.push(arr[idx + 1]);
+      }
+    } else if (arr[idx] === '--double-prev') {
+      if (arr[idx - 1] && arr[idx - 2] !== '--discard-next') {
+        newArr.push(arr[idx - 1]);
+      }
+    } else {
+      newArr.push(arr[idx]);
+    }
+  }
+  return newArr;
 }
+
+// function transform(arr) {
+//   if (!Array.isArray(arr)) {
+//     throw new Error ("'arr' parameter must be an instance of the Array!");
+//   }
+//   if (arr.length === 0) {
+//     return [];
+//   }
+
+//   const newArr = [];
+//   for (let idx = 0; idx < arr.length; idx++) {
+//     if (arr[idx] === '--discard-next') {
+//       if (arr[idx + 1]) {
+//         console.log('here');
+//         idx++;
+//       }
+//     } else if (arr[idx] === '--discard-prev') {
+//       if (arr[idx - 1] && arr[idx - 2] !== '--discard-next') {
+//         newArr.pop();
+//       }
+//     } else if (arr[idx] === '--double-next') {
+//       if (arr[idx + 1]) {
+//         newArr.push(arr[idx + 1]);
+//       }
+//     } else if (arr[idx] === '--double-prev') {
+//       if (arr[idx - 1] && arr[idx - 2] !== '--discard-next') {
+//         newArr.push(arr[idx - 1]);
+//       }
+//     } else {
+//       newArr.push(arr[idx]);
+//     }
+//   }
+//   return newArr;
+// }
