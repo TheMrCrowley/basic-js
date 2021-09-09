738ab777ccc987370976ad5f8cf144f83cb75927
Denis Mythnik <themrcrovvley@gmail.com>
2021-09-09 10:13:49 +0300

    feat: add solve to dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    return members.map(item => {
+      if (typeof item === 'string') {
+        item = item.replace(new RegExp(/\s/, "g"), '');
+        return item[0].toUpperCase();
+      }
+    }).sort().join('');
+  }
+  return false;
 }
