97704824a368ba6370ddddd107b461f76b0a4de2
Denis Mythnik <themrcrovvley@gmail.com>
2021-09-09 17:44:03 +0300

    feat: first solve to reverse-depth task, maybe change

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,16 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let arrCounter = 1;
+    let arrDeep = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        arrCounter = this.calculateDepth(arr[i]);
+        arrDeep = Math.max(arrDeep, arrCounter);
+      }
+    }
+    arrDeep++;
+    return arrDeep;
   }
 }
