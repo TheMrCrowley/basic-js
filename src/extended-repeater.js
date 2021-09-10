c72693bfcdab9034c5a5ea8141d06cef529a95ce
Denis Mythnik <themrcrovvley@gmail.com>
2021-09-10 14:03:48 +0300

    feat: add solve to extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,43 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  return repeat(str, options.repeatTimes);
+  function repeat(str, num) {
+    if (!num) {
+      return str + additionRepeat(options.additionRepeatTimes);
+    }
+    if (num === 1) {
+      return str + additionRepeat(options.additionRepeatTimes);;
+    }
+    return str + additionRepeat(options.additionRepeatTimes) + separator(options) + repeat(str, num - 1);
+  }
+
+  function separator(path) {
+    if ('separator' in path) {
+      return path.separator;
+    }
+    return '+';
+  }
+
+  function addition(path) {
+    if ('addition' in path) {
+      return path.addition;
+    }
+    return '';
+  }
+  
+  function additionSeparator(path) {
+    if ('additionSeparator' in options) {
+      return path.additionSeparator;
+    }
+    return '|';
+  }
+  
+  function additionRepeat(num) {
+    if (!num || num === 1) {
+      return addition(options);
+    }
+    return addition(options) + additionSeparator(options) + additionRepeat(num - 1);
+  }
 }
