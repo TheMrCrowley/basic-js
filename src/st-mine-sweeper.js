8ddd408e827f5914d876839939e92cf787f73b0d
TheMrCrowley <themrcrovvley@gmail.com>
2021-09-11 13:43:16 +0300

    feat: add solve to mine-sweeper task

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,39 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  const result = matrix.map(line => line.map(token => 0));
+  for (let i = 0; i< matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1]) {
+        if (matrix[i - 1][j - 1]) {
+          result[i][j]++;
+        }
+        if (matrix[i - 1][j]) {
+          result[i][j]++;
+        }
+        if (matrix[i - 1][j + 1]) {
+          result[i][j]++;
+        }
+      }
+      if (matrix[i + 1]) {
+        if (matrix[i + 1][j - 1]) {
+          result[i][j]++;
+        }
+        if (matrix[i + 1][j]) {
+          result[i][j]++;
+        }
+        if (matrix[i + 1][j + 1]) {
+          result[i][j]++;
+        }
+      }
+      if (matrix[i][j - 1]) {
+        result[i][j]++;
+      }
+      if (matrix[i][j + 1]) {
+        result[i][j]++;
+      }      
+    }
+  }
+  return result;
 }
